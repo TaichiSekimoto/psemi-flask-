76a88e0347a653f287b62c593a1dbe202b9878a8
TaichiSekimoto <21fi063@ms.dendai.ac.jp>
2023-12-31 22:04:38 +0900

    コミットメッセージ

--- app.py
+++ app.py
@@ -24,14 +24,41 @@ def index():
  tasks=Todo.query.all()
  return render_template("index.html", tasks=tasks)
 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+## /update/1　に来たときの処理を実装する
+def update(id):
+    ## idをもとにデータベースからタスクを取得する
+    updatetask= Todo.query.get(id)
+    
+    ## GETのときはupdate.htmlを表示する
+    if  request.method == 'GET':
+        return render_template('update.html',task=updatetask)
+    ## update.htmlには元々のタスクの情報を表示したい
+    
+
+    
+    ## POSTのときはデータベースを更新して/にリダイレクトする
+    elif request.method == 'POST':
+         title = request.form.get("title")
+         details = request.form.get("details")
+         updatetask.title=title
+         updatetask.details=details
+         db.session.add(updatetask)
+         db.session.commit()
+
+         return redirect('/')
+    ## データベースを更新するときは、タスクを取得→取得したタスクの内容を更新→保存する
+    
+    
 @app.route('/delete/<int:id>')
 def delete(id):
     delete_task = Todo.query.get(id)
     db.session.delete(delete_task)
     db.session.commit()
+
     return redirect('/')
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-
+   with app.app_context():
+        db.create_all()
+   app.run(debug=True)
